22f7e97cb479119d97d456dc6e7a48f15c5ae9f4
username <email>
2019-02-05 07:30:51 +0100

    commit 1

---
+++ script/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("menu").addEventListener("click", () => {
+    document.getElementById("list").classList.toggle("active");
+  });
+});
+
+function klick() {
+  console.log("test1");
+}
